f01daf32a59e9058a02a805a217a53ec97c4a75e
Laura Walters <laura.walters1098@yahoo.com>
2023-05-25 22:47:05 +0100

    adding required test cases

--- calculatePacks/calculatePacks.go
+++ calculatePacks/calculatePacks.go
@@ -1,5 +1,19 @@
 package calculatepacks
 
-func calculatepacks(count int32) int32 {
-	return 1
+type OrderBreakdown struct {
+	_250  int32
+	_500  int32
+	_1000 int32
+	_2000 int32
+	_5000 int32
+}
+
+func calculatepacks(count int32) OrderBreakdown {
+	return OrderBreakdown{
+		_250:  1,
+		_500:  0,
+		_1000: 0,
+		_2000: 0,
+		_5000: 0,
+	}
 }
--- calculatePacks/calculatePacks_test.go
+++ calculatePacks/calculatePacks_test.go
@@ -2,12 +2,82 @@ package calculatepacks
 
 import "testing"
 
-func TestCalculatePacks(t *testing.T) {
-	var expected int32 = 1
+func TestCalculatePacks1(t *testing.T) {
+	expected := OrderBreakdown{
+		_250:  1,
+		_500:  0,
+		_1000: 0,
+		_2000: 0,
+		_5000: 0,
+	}
 	result := calculatepacks(1)
 	if result != expected {
-		t.Errorf("FAILED - Expected %d GOT %d", expected, result)
+		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
 	} else {
-		t.Logf("PASSED - Expected %d GOT %d", expected, result)
+		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+	}
+}
+
+func TestCalculatePacks250(t *testing.T) {
+	expected := OrderBreakdown{
+		_250:  1,
+		_500:  0,
+		_1000: 0,
+		_2000: 0,
+		_5000: 0,
+	}
+	result := calculatepacks(250)
+	if result != expected {
+		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	} else {
+		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+	}
+}
+
+func TestCalculatePacks251(t *testing.T) {
+	expected := OrderBreakdown{
+		_250:  0,
+		_500:  1,
+		_1000: 0,
+		_2000: 0,
+		_5000: 0,
+	}
+	result := calculatepacks(1)
+	if result != expected {
+		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	} else {
+		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+	}
+}
+
+func TestCalculatePacks501(t *testing.T) {
+	expected := OrderBreakdown{
+		_250:  1,
+		_500:  1,
+		_1000: 0,
+		_2000: 0,
+		_5000: 0,
+	}
+	result := calculatepacks(1)
+	if result != expected {
+		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	} else {
+		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+	}
+}
+
+func TestCalculatePacks12001(t *testing.T) {
+	expected := OrderBreakdown{
+		_250:  1,
+		_500:  0,
+		_1000: 0,
+		_2000: 1,
+		_5000: 2,
+	}
+	result := calculatepacks(1)
+	if result != expected {
+		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	} else {
+		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
 	}
 }
