ee11d8a12d42c4cbd8b45a0c7ee96be57c83f4d7
Laura Walters <laura.walters1098@yahoo.com>
2023-05-25 21:54:56 +0100

    created an endpoint that accepts a body

--- main.go
+++ main.go
@@ -6,12 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Order struct {
+	Count int32 `json:"count"`
+}
+
 func sayHi(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, "Hey!")
+}
+
+func placeOrder(context *gin.Context) {
+	var order Order
+
+	context.BindJSON(&order)
+
+	context.IndentedJSON(http.StatusOK, order)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/hi", sayHi)
+	router.GET("/place-order", placeOrder)
 	router.Run(":3000")
 }
