867d94780b0afdd1816b39e710c272a765759513
Laura Walters <laura.walters1098@yahoo.com>
2023-05-26 13:47:09 +0100

    identifies all pack size combos to meet count

--- calculatePacks/calculatePacks.go
+++ calculatePacks/calculatePacks.go
@@ -6,8 +6,23 @@ import (
 	"sort"
 )
 
-func recursive(innerSize int, packSizes []int, sizes []int, count int, packSizesNeeded [][]int) {
-
+func recursive(allPossibleCalcs [][]int, count int, packSizes []int, accumulatedSizes []int) []int {
+	var sum int
+	for _, valueInt := range accumulatedSizes {
+		sum += valueInt
+	}
+	// fmt.Println(accumulatedSizes)
+	if sum >= count {
+		// allPossibleCalcs = accumulatedSizes
+		// fmt.Println(allPossibleCalcs)
+		return accumulatedSizes
+	} else {
+		for _, size := range packSizes {
+			accumulatedSizes = append(accumulatedSizes, size)
+			return recursive(allPossibleCalcs, count, packSizes, accumulatedSizes)
+		}
+	}
+	return accumulatedSizes
 }
 
 func orderPackSizes(order map[int]int) []int {
@@ -55,14 +70,23 @@ func CalculatePacks(count int) map[int]int {
 
 	// GET HOW MANY OF EACH SIZE WOULD BE REQUIRED IF YOU COULD ONLY TAKE ONE SIZE
 	var requiredNumsForEachSize = getNumberRequiredForEachSize(packSizes, count)
+
+	// This variable will store arrays which contain all the number combinations that fulfill the order
+	var allPossibleCalculations = [][]int{}
 	fmt.Println(requiredNumsForEachSize)
 	if allValuesEqualOne(requiredNumsForEachSize) {
 		order[packSizes[0]]++
 		return order
 	} else {
+		for _, size := range packSizes {
+			acc := []int{size}
+			var allCalcs []int = recursive(allPossibleCalculations, count, packSizes, acc)
+			allPossibleCalculations = append(allPossibleCalculations, allCalcs)
+			fmt.Println(allCalcs)
+
+		}
+		fmt.Println("allPossibleCalculations", allPossibleCalculations)
 		return order
 	}
-
-	// fmt.Println(requiredNumsForEachSize)
 
 }
