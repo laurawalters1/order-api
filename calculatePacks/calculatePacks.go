b531d4257641b85cd24c24975fa5c09d0548d0f2
Laura Walters <laura.walters1098@yahoo.com>
2023-05-26 07:29:50 +0100

    modifying tests

--- calculatePacks/calculatePacks.go
+++ calculatePacks/calculatePacks.go
@@ -1,19 +1,13 @@
 package calculatepacks
 
-type OrderBreakdown struct {
-	_250  int32
-	_500  int32
-	_1000 int32
-	_2000 int32
-	_5000 int32
-}
-
-func calculatepacks(count int32) OrderBreakdown {
-	return OrderBreakdown{
-		_250:  1,
-		_500:  0,
-		_1000: 0,
-		_2000: 0,
-		_5000: 0,
+func calculatepacks(count int32) map[int32]int {
+	order := map[int32]int{
+		250:  1,
+		500:  0,
+		1000: 0,
+		2000: 0,
+		5000: 0,
 	}
+
+	return order
 }
--- calculatePacks/calculatePacks_test.go
+++ calculatePacks/calculatePacks_test.go
@@ -1,83 +1,91 @@
 package calculatepacks
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCalculatePacks1(t *testing.T) {
-	expected := OrderBreakdown{
-		_250:  1,
-		_500:  0,
-		_1000: 0,
-		_2000: 0,
-		_5000: 0,
+	expected := map[int32]int{
+		250:  1,
+		500:  0,
+		1000: 0,
+		2000: 0,
+		5000: 0,
 	}
+
 	result := calculatepacks(1)
-	if result != expected {
-		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\033[31mFAILED - Expected %%v: %v GOT %%v: %v\033[0m", expected, result)
 	} else {
-		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+		t.Logf("\033[32mPASSED - Expected %v GOT %v\033[0m", expected, result)
 	}
 }
 
 func TestCalculatePacks250(t *testing.T) {
-	expected := OrderBreakdown{
-		_250:  1,
-		_500:  0,
-		_1000: 0,
-		_2000: 0,
-		_5000: 0,
+	expected := map[int32]int{
+		250:  1,
+		500:  0,
+		1000: 0,
+		2000: 0,
+		5000: 0,
 	}
+
 	result := calculatepacks(250)
-	if result != expected {
-		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\033[31mFAILED - Expected %v GOT %v\033[0m", expected, result)
 	} else {
-		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+		t.Logf("\033[32mPASSED - Expected %v GOT %v\033[0m", expected, result)
 	}
 }
 
 func TestCalculatePacks251(t *testing.T) {
-	expected := OrderBreakdown{
-		_250:  0,
-		_500:  1,
-		_1000: 0,
-		_2000: 0,
-		_5000: 0,
+	expected := map[int32]int{
+		250:  0,
+		500:  1,
+		1000: 0,
+		2000: 0,
+		5000: 0,
 	}
+
 	result := calculatepacks(1)
-	if result != expected {
-		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\033[31mFAILED - Expected %v GOT %v\033[0m", expected, result)
 	} else {
-		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+		t.Logf("\033[32mPASSED - Expected %v GOT %v\033[0m", expected, result)
 	}
 }
 
 func TestCalculatePacks501(t *testing.T) {
-	expected := OrderBreakdown{
-		_250:  1,
-		_500:  1,
-		_1000: 0,
-		_2000: 0,
-		_5000: 0,
+	expected := map[int32]int{
+		250:  1,
+		500:  1,
+		1000: 0,
+		2000: 0,
+		5000: 0,
 	}
+
 	result := calculatepacks(1)
-	if result != expected {
-		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\033[31mFAILED - Expected %v GOT %v\033[0m", expected, result)
 	} else {
-		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+		t.Logf("\033[32mPASSED - Expected %v GOT %v\033[0m", expected, result)
 	}
 }
 
 func TestCalculatePacks12001(t *testing.T) {
-	expected := OrderBreakdown{
-		_250:  1,
-		_500:  0,
-		_1000: 0,
-		_2000: 1,
-		_5000: 2,
+	expected := map[int32]int{
+		250:  1,
+		500:  0,
+		1000: 0,
+		2000: 1,
+		5000: 2,
 	}
+
 	result := calculatepacks(1)
-	if result != expected {
-		t.Errorf("\033[31mFAILED - Expected %d GOT %d\033[0m", expected, result)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("\033[31mFAILED - Expected %v GOT %v\033[0m", expected, result)
 	} else {
-		t.Logf("\033[32mPASSED - Expected %d GOT %d\033[0m", expected, result)
+		t.Logf("\033[32mPASSED - Expected %v GOT %v\033[0m", expected, result)
 	}
 }
