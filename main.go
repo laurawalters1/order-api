9d1676db9ea79c805ee1832d55884befb1da5b9f
Laura Walters <laura.walters1098@yahoo.com>
2023-05-26 19:42:43 +0100

    removing calculate packs

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/laurawalters1/order-api/calculatepacks"
+	// "github.com/laurawalters1/order-api/calculatePacks"
 )
 
 type Order struct {
@@ -20,9 +20,9 @@ func placeOrder(context *gin.Context) {
 	var order Order
 	context.BindJSON(&order)
 	fmt.Println("Count", order.Count)
-	var totals = calculatepacks.CalculatePacks(int(order.Count))
+	// var totals = calculatePacks.CalculatePacks(int(order.Count))
 
-	context.IndentedJSON(http.StatusOK, totals)
+	context.IndentedJSON(http.StatusOK, "hello")
 }
 
 func main() {
